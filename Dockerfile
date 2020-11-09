5ed305865d5e2a87d40612acf6af6aba707a9a16
Lukas Lindqvist <lukas.lndqvst@gmail.com>
2020-11-09 17:59:54 +0100

    Change to use ifupdown

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,8 @@
-FROM alpine:3.11.5 as base
-
-FROM base as builder
+FROM alpine:3.12 as base
 
 RUN apk update && \
     apk add --no-cache \
-    build-base linux-headers \
-    iproute2 iptables ip6tables
-
-RUN mkdir -p /build
-
-COPY wireguard-tools /wireguard-tools
-
-ENV DESTDIR /build
-ENV WITH_SYSTEMDUNITS no
-ENV WITH_BASHCOMPLETION no
-ENV WITH_WGQUICK yes
-
-WORKDIR /wireguard-tools/src
-
-RUN make install
-
-FROM base
-
-COPY --from=builder /build /
-
-RUN apk update && \
-    apk add --no-cache \
-    bash iproute2 openresolv ip6tables iptables
-
+    bash iproute2 openresolv ip6tables iptables wireguard-tools-wg
 
 COPY docker-entrypoint.sh /usr/bin/docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,24 +4,27 @@
 ## The below is modified from https://github.com/activeeos/wireguard-docker
 
 # Find a Wireguard interface
-interfaces=`find /etc/wireguard -type f`
-if [[ -z $interfaces ]]; then
-    echo "$(date): Interface not found in /etc/wireguard" >&2
-    exit 1
-fi
+# interfaces=`find /etc/wireguard -type f`
+# if [[ -z $interfaces ]]; then
+#     echo "$(date): Interface not found in /etc/wireguard" >&2
+#     exit 1
+# fi
+cat /config/interfaces >> /etc/network/interfaces
 
 
-for interface in $interfaces; do
-    echo "$(date): Starting Wireguard $interface"
-    wg-quick up $interface
-done
+ifup wg0
+# for interface in $interfaces; do
+#     echo "$(date): Starting Wireguard $interface"
+#     wg-quick up $interface
+# done
 
 # Handle shutdown behavior
 finish () {
     echo "$(date): Shutting down Wireguard"
-    for interface in $interfaces; do
-        wg-quick down $interface
-    done
+    # for interface in $interfaces; do
+    #     wg-quick down $interface
+    # done
+    ifdown wg0
 
     exit 0
 }
